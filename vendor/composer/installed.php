0eb06b09c24baae812e5d5c13f0a2f3f7dd35cc9
hetaocuc <andy_he@hotmail.com>
2022-03-05 20:11:37 -0500

    Heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8acc831c239aa65c1c34c5a9db92d84ea850bcde',
+        'name' => 'hetao/example',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hetao/example' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8acc831c239aa65c1c34c5a9db92d84ea850bcde',
+            'dev_requirement' => false,
+        ),
+    ),
+);
